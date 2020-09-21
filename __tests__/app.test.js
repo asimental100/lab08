99c994cea8727d3057ed5f79c12a225f99a5c3d5
Adrian Simental <asimental100@hotmail.com>
2020-09-21 14:07:04 -0700

    creatresponse

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -2,5 +2,9 @@ const request = require('supertest');
 const app = require('../lib/app');
 
 describe('createResponse', () => {
+  it('should test / route', () => {
 
+  });
+
+  expect(true).toEqual(true);
 });
