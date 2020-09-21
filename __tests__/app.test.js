ec61e19aa92780545c6b449a5a16bcc2a1492fe1
Adrian Simental <asimental100@hotmail.com>
2020-09-21 14:32:18 -0700

    seven tests passing

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -1,10 +1,29 @@
 const request = require('supertest');
 const app = require('../lib/app');
 
-describe('createResponse', () => {
-  it('should test / route', () => {
+describe('testing routes', () => {
+  it('should test / route', async() => {
+    const response = await request(app).get('/');
 
+    expect(response.text).toEqual('<html><body><h1>hi</h1></body></html>');
   });
 
-  expect(true).toEqual(true);
+  it('should test /red route', async() => {
+    const response = await request(app).get('/red');
+
+    expect(response.text).toEqual('<html><body><h1>red</h1></body></html>');
+  });
+
+  it('should test /green route', async() => {
+    const response = await request(app).get('/green');
+
+    expect(response.text).toEqual('<html><body><h1>green</h1></body></html>');
+  });
+
+  it('should test /blue route', async() => {
+    const response = await request(app).get('/blue');
+
+    expect(response.text).toEqual('<html><body><h1>blue</h1></body></html>');
+  });
+
 });
